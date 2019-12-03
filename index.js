a663d4c42c4788fc98c2d8fba5bf0d6f7c0c815d
Dimpy <dimpy13parekh@gmail.com>
2019-12-02 22:14:05 -0500

    modified code

--- index.js
+++ index.js
@@ -1,59 +1,61 @@
-//Months
-$(document).ready(function() {
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
-    let selectMonth = $("#month");
-    for (let m = 0; m < 12; m++) {
-        let monthNum = new Date(2018, m).getMonth()
-        let month = monthNames[monthNum];
-        let monthElem = document.createElement("option");
-        monthElem.value = monthNum;
-        monthElem.textContent = month;
-        selectMonth.append(monthElem);
-    }
-});
-
-//Gross Salary
 let btn = document.getElementById("btn");
 
-function findGrossSalary() {
-    let num = parseFloat(document.getElementById("num").value);
-    let name = document.getElementById("name").value;
-    let dept = document.getElementById("dept").value;
+let num = parseFloat(document.getElementById("num").value);
+let name = document.getElementById("name").value;
+let dept = document.getElementById("dept").value;
+let empCode = document.getElementById("empCode").value;
+let qCode = document.getElementById("qualiCode");
+let grossSalary = parseFloat(document.getElementById("grossSalary"));
+let hourlyRate = parseFloat(document.getElementById("hourlyRate"));
+let excessHours = parseFloat(document.getElementById("excessHours"));
+let incomeTax = parseFloat(document.getElementById("incomeTax"));
+let totalDeduction = parseFloat(document.getElementById("totalDeduction"));
+let netPay = parseFloat(document.getElementById("netPay"));
+
+function netPayable() {
+    //gross salary
     let hours = parseFloat(document.getElementById("hours").value);
-    let empCode = document.getElementById("empCode").value;
     let fixSal = parseFloat(document.getElementById("fixSal").value);
-    let grossSalary = parseFloat(document.getElementById("grossSalary"));
-    let hourlyRate = parseFloat(document.getElementById("hourlyRate"));
-    let excessHours = parseFloat(document.getElementById("excessHours"));
-
-
-    let qCode = document.getElementById("qualiCode");
 
     if (empCode.selectedIndex == 0) {
         if (qCode.selectedIndex == 0) {
             grossSalary = (175 * hours) + 1500;
-            alert(`${grossSalary}`);
+            // alert(`${grossSalary}`);
         } else {
             grossSalary = (100 * hours) + 600;
-            alert(`${grossSalary}`);
+            // alert(`${grossSalary}`);
         }
     } else {
         if (hours == 160) {
-            alert(`${fixSal}`);
+            // alert(`${fixSal}`);
 
         } else if (hours < 160) {
             hourlyRate = fixSal / 160;
             grossSalary = hourlyRate * hours;
-            alert(`${grossSalary}`);
+            // alert(`${grossSalary}`);
         } else {
-            let excessHours = (hours - 160);
+            excessHours = (hours - 160);
             hourlyRate = fixSal / 160;
             grossSalary = ((hourlyRate * 160) + (excessHours * hourlyRate * 2));
-            alert(`${grossSalary}`);
+            // alert(`${grossSalary}`);
+
+
+            //deduction
+            incomeTax = ((grossSalary * 25) / 100).toFixed(2);
+
+            if (grossSalary > 3000) {
+                totalDeduction = incomeTax + 33;
+            } else {
+                totalDeduction = incomeTax + 19.20;
+            }
+
+            //net payable
+            netPay = grossSalary - totalDeduction;
 
         }
     }
 }
-btn.addEventListener("click", findGrossSalary);
+
+
+
+btn.addEventListener("click", netPayable);
