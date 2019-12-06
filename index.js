29d32b8e4ca39dd384d04cefbd75080167000818
Dimpy <dimpy13parekh@gmail.com>
2019-12-06 17:16:26 -0500

    designing done

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let dept = document.getElementById("dept");
 let empCode = document.getElementById("empCode");
 let qCode = document.getElementById("qualiCode");
 let grossSalary = document.getElementById("grossSalary");
-let hourlyRate = (document.getElementById("hourlyRate"));
-let excessHours = (document.getElementById("excessHours"));
-let incomeTax = (document.getElementById("incomeTax"));
-let totalDeduction = (document.getElementById("totalDeduction"));
-let netPay = (document.getElementById("netPay"));
+let hourlyRate = document.getElementById("hourlyRate");
+let excessHours = document.getElementById("excessHours");
+let incomeTax = document.getElementById("incomeTax");
+let totalDeduction = document.getElementById("totalDeduction");
+let netPay = document.getElementById("netPay");
 let hours = document.getElementById("hours");
 let fixSal = document.getElementById("fixSal");
 
@@ -19,7 +19,6 @@ document.getElementById(`empCode`).addEventListener(`change`, () => {
 });
 
 function output() {
-
     function calcGrossSalary() {
         //gross salary
         if (empCode.selectedIndex == 0) {
@@ -58,42 +57,14 @@ function output() {
         netPay.innerHTML = parseFloat(grossSalary.innerHTML) - parseFloat(totalDeduction.innerHTML);
     }
 
-    // return `<article>
-    // <h2>Pay Slip</h2>
-    //         <label> Employee Number :</label>
-    //         <label>${num}</label><br><br>
-    //         <label> Employee Name :</label>
-    //         <label>${name}</label><br><br>
-    //         <label> Department :</label>
-    //         <label>${dept}</label><br><br>
-    //         <label> Employee Type Code :</label>
-    //         <label>${empCode}</label><br><br>
-    //         <label> Working hours :</label>
-    //         <label>${hours}</label><br><br>
-    //         <label>  Gross Salary :</label>
-    //         <label>${grossSalary}</label><br><br>
-    //         <label> Deduction :</label>
-    //         <label>${totalDeduction}</label><br><br>
-    //         <label>  Net Salary :</label>
-    //         <label>${netPay}</label><br><br>
-    //         </article>`;
     document.getElementById(`numOutput`).innerText = num.value;
     document.getElementById(`nameOutput`).innerText = name.value;
     document.getElementById(`deptOutput`).innerText = dept.value;
     document.getElementById(`empCodeOutput`).innerText = empCode.value;
     document.getElementById(`hoursOutput`).innerText = hours.value;
-    // document.getElementById(`grossSalaryOutput`) = grossSalary;
-    // document.getElementById(`totalDeductionOutput`) = totalDeduction;
-    // document.getElementById(`netPayOutput`) = netPay;
-
+    document.getElementById(`grossSalaryOutput`).innerHTML = grossSalary;
+    document.getElementById(`totalDeductionOutput`).innerHTML = totalDeduction;
+    document.getElementById(`netPayOutput`).innerHTML = netPay;
 }
 
 btn.addEventListener("click", output);
-
-
-// 1.add an event listener to the button when clicked (get payslip)
-// 2. get input values from form
-//3. now that you have the form values, calculate for the payslip data // calculate function
-// 4. inside cacl function you can also have another function to get/calc deductions
-// 5. now that you have your net income print them to the html page
-//
