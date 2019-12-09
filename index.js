f08010c762bd485baa5e968add894c040052020e
Dimpy <dimpy13parekh@gmail.com>
2019-12-09 17:55:54 -0500

    done

--- index.js
+++ index.js
@@ -1,74 +1,74 @@
 let btn = document.getElementById("btn");
 
-let num = document.getElementById("num");
-let name = document.getElementById("name");
-let dept = document.getElementById("dept");
-let empCode = document.getElementById("empCode");
-let qCode = document.getElementById("qualiCode");
-let grossSalary = document.getElementById("grossSalary");
-let hourlyRate = document.getElementById("hourlyRate");
-let excessHours = document.getElementById("excessHours");
-let incomeTax = document.getElementById("incomeTax");
-let totalDeduction = document.getElementById("totalDeduction");
-let netPay = document.getElementById("netPay");
-let hours = document.getElementById("hours");
-let fixSal = document.getElementById("fixSal");
-
+//toggling clss from hidden to show when Employee type is Regular worker
 document.getElementById(`empCode`).addEventListener(`change`, () => {
     document.getElementById('fixSalDiv').classList.toggle('show');
 });
 
-function output() {
-    function calcGrossSalary() {
-        //gross salary
-        if (empCode.selectedIndex == 0) {
-            if (qCode.selectedIndex == 0) {
-                grossSalary.innerHTML = (175 * parseFloat(hours.innerHTML)) + 1500;
-            } else {
-                grossSalary.innerHTML = (100 * parseFloat(hours.innerHTML)) + 600;
-            }
+function calcGrossSalary() {
+    let empCode = document.getElementById("empCode");
+    let qCode = document.getElementById("qualiCode");
+    let hours = document.getElementById("hours");
+    let fixSal = document.getElementById("fixSal");
+    let grossSalary;
+
+    //gross salary
+    if (empCode.selectedIndex == 0) {
+        if (qCode.selectedIndex == 0) {
+            grossSalary = (175 * parseFloat(hours.value)) + 1500;
         } else {
-            document.getElementById('fixSalDiv').classList.toggle('show');
-            if (hours.innerHTML == 160) {
-                grossSalary.innerHTML = grossSalary.innerHTML;
-            } else if (hours.innerHTML < 160) {
-                hourlyRate.innerHTML = (parseFloat(fixSal.innerHTML) / 160).toFixed(2);
-                grossSalary.innerHTML = parseFloat(hourlyRate.innerHTML * hours.innerHTML);
-            } else {
-                excessHours.innerHTML = parseFloat(hours.innerHTML) - 160;
-                hourlyRate.innerHTML = (parseFloat(fixSal.innerHTML) / 160).toFixed(2);
-                grossSalary.innerHTML = ((parseFloat(hourlyRate.innerHTML) * 160) + (excessHours.innerHTML * (hourlyRate.innerHTML) * 2));
-            }
+            grossSalary = (100 * parseFloat(hours.value)) + 600;
+        }
+    } else {
+        document.getElementById('fixSalDiv').classList.toggle('show');
+        if (hours.value == 160) {
+            grossSalary = grossSalary.value;
+        } else if (hours.value < 160) {
+            hourlyRate = (parseFloat(fixSal.value) / 160).toFixed(2);
+            grossSalary = parseFloat(hourlyRate * hours.value);
+        } else {
+            excessHours = parseFloat(hours.value) - 160;
+            hourlyRate = (parseFloat(fixSal.value) / 160).toFixed(2);
+            grossSalary = ((parseFloat(hourlyRate) * 160) + (excessHours * (hourlyRate) * 2));
         }
     }
+    return grossSalary;
+}
 
-    function calcDeduction() {
-        //deduction
-        const incomeTaxPercentage = 25;
-
-        incomeTax.innerHTML = (parseFloat(grossSalary.innerHTML * incomeTaxPercentage) / 100).toFixed(2);
-        if (grossSalary.innerHTML > 3000) {
-            totalDeduction.innerHTML = parseFloat(incomeTax.innerHTML) + 33;
-        } else if (grossSalary.innerHTML < 2500) {
-            totalDeduction.innerHTML = 19.20;
-        } else {
-            totalDeduction.innerHTML = parseFloat(incomeTax.innerHTML) + 19.20;
-        }
+function calcDeduction(grossSalary) {
+    //deduction
+    const incomeTaxPercentage = 25;
+    const incomeTax = (parseFloat(grossSalary * incomeTaxPercentage) / 100).toFixed(2);
+    let totalDeduction;
+    if (grossSalary > 3000) {
+        totalDeduction = parseFloat(incomeTax) + 33;
+    } else if (grossSalary < 2500) {
+        totalDeduction = 19.20;
+    } else {
+        totalDeduction = parseFloat(incomeTax) + 19.20;
     }
+    return totalDeduction;
+}
 
-    function calcNetPay() {
-        //net payable
-        netPay.innerHTML = parseFloat(grossSalary.innerHTML) - parseFloat(totalDeduction.innerHTML);
-    }
+function calcNetPay() {
+    let num = document.getElementById("num");
+    let name = document.getElementById("name");
+    let dept = document.getElementById("dept");
 
+    //net payable
+    const grossSalary = calcGrossSalary();
+    const deductions = calcDeduction(grossSalary);
+    netPay = grossSalary - deductions;
+
+    //output
     document.getElementById(`numOutput`).innerText = num.value;
     document.getElementById(`nameOutput`).innerText = name.value;
     document.getElementById(`deptOutput`).innerText = dept.value;
     document.getElementById(`empCodeOutput`).innerText = empCode.value;
     document.getElementById(`hoursOutput`).innerText = hours.value;
     document.getElementById(`grossSalaryOutput`).innerHTML = grossSalary;
-    document.getElementById(`totalDeductionOutput`).innerHTML = totalDeduction;
+    document.getElementById(`totalDeductionOutput`).innerHTML = deductions;
     document.getElementById(`netPayOutput`).innerHTML = netPay;
 }
 
-btn.addEventListener("click", output);
+btn.addEventListener("click", calcNetPay);
